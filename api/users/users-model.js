7fc0bdab8ffc99c968e5efc173da1a2ffcf40ad1
Richard Oh <n24512@protonmail.com>
2022-01-19 15:21:15 -0600

    creates migrations table

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -4,22 +4,28 @@ const findAll = () => {
   return db("users_table");
 };
 
-const findById = (id) => {
-  return db("users_table").where("user_id", id).first();
-};
-
 const findBy = (filter) => {
-  return db("users_table").select("user_id", "password").where(filter);
+  return db("users_table").select("user_id", "user_email").where(filter);
 };
 
 const addUser = async (user) => {
-  const newUser = await db("users_table").insert(user, ["email"]);
+  const newUser = await db("users_table").insert(user, [
+    "user_email",
+    "user_password",
+  ]);
   return newUser;
+};
+
+const updateUser = async (id, changes) => {
+  const updatedUser = await db("users_table")
+    .where("user_id", id)
+    .update(changes, ["user_email", "user_password"]);
+  return updatedUser;
 };
 
 module.exports = {
   findAll,
-  findById,
   findBy,
   addUser,
+  updateUser,
 };
